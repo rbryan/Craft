36445c03f806803a6f389af046811b1a1445f0cb
Russell Bryan <bryanr@ou.edu>
2015-10-25 02:32:07 -0500

    Yay, loading config from home directory.

--- src/script.c
+++ src/script.c
@@ -4,6 +4,7 @@
 sexp chibi_context;
 
 void init_chibi_context(){
+
 	chibi_context = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
 	sexp_load_standard_env(chibi_context, NULL, SEXP_SEVEN);
 	sexp_load_standard_ports(chibi_context, NULL, stdin, stdout, stderr, 0);
@@ -12,5 +13,13 @@ void init_chibi_context(){
 
 	sexp_init_library(chibi_context, NULL, 3, env, sexp_version, SEXP_ABI_IDENTIFIER);
 
-	sexp_eval_string(chibi_context, "(add-message \"haha\")", -1, NULL);
+	sexp_gc_var1(config_path);
+	sexp_gc_preserve1(chibi_context, config_path);
+
+	config_path = sexp_c_string(chibi_context, "/home/russell/.craftconf.scm", -1);
+
+	sexp_load(chibi_context, config_path, NULL);
+
+	sexp_gc_release1(chibi_context);
+
 }
