ea26602b9d96c01fdcefc4df85e2d47bb750b52f
Russell Bryan <bryanr@ou.edu>
2015-10-25 15:55:48 -0500

    added craftconf.scm

---
+++ craftconf.scm
@@ -0,0 +1,17 @@
+(define-syntax loopvar
+  (syntax-rules ()
+		((_ var a b exp ...) (let ((inc (if (> a b) - +)))
+					     (do ((var a (inc var 1))) ((= var b) exp ...) exp ...)))))
+
+(define (display-message thing)
+  (add-message
+    (let ((output-port (open-output-string)))
+      (display thing output-port)
+      (get-output-string output-port))))
+
+(define (print . stuff)
+  (map display-message stuff)
+  #t)
+		  
+
+
